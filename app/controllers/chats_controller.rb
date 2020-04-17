2c2b7f91573fddacc5edb0671bd3483a51bd4317
nkizito92 <nkizito57@gmail.com>
2020-04-17 11:50:28 -0500

    refactored and added a guest build or find condition

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,21 +1,26 @@
 class ChatsController < ApplicationController
     def index 
         chats = Chat.all 
-        render json: chats, include: [:guest, :image]
+        render json: chats, include: [:guest]
     end 
 
     def show 
         chat = Chat.find_by_id(params[:id])
-        render json: chat, include: [:guest, :image]
+        render json: chat, include: [:guest]
     end
 
-    def create 
-       chat = Chat.create(message: params[:message])
-       chat.img = params[:img] if !params[:img].nil?
+    def create   
+        chat = Chat.new(message: chat_params[:message], img: chat_params[:img])
+         if !chat_params[:name].nil?
+            exsitingGuest = Guest.find_by(name: chat_params[:name])
+           !exsitingGuest.nil? ? (exsitingGuest.chats << chat ) : (chat.build_guest({name: chat_params[:name]}) && chat.save)
+         end 
+       render json: chat
     end
 
     def edit 
         chat = Chat.find_by_id(params[:id])
+        render json: chat
     end 
 
     def update 
@@ -27,5 +32,10 @@ class ChatsController < ApplicationController
     def destroy 
         chat = Chat.find_by_id(params[:id])
         chat.delete
+    end
+
+    private 
+    def chat_params
+        params.require("chat").permit(:message, :img, :name)
     end
 end

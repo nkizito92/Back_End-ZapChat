5439ddebbd1f3c9976edc5f76a6a56004677a062
nkizito92 <nkizito57@gmail.com>
2020-05-14 16:35:13 -0500

    added a user model for login in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,30 @@ class UsersController < ApplicationController
         users = User.all
         render json: users, include: [:chats]
     end 
-    
-    def create 
+
+    def show 
+        exiting = current_user
     end 
     
-    def update 
-
+    def create 
+        user = User.create(user_params)
+        session[:user_id] = user.id
+        render json: user,  include: [:chats]
     end 
 
     def destroy 
+        user = User.find_by_id(params[:id])
+        user.delete
+        render json: {id: params[:id]}
+    end 
 
+    def logout
+        session.clear
+    end
+
+    private 
+
+    def user_params 
+        params.require("user").permit(:username, :password)
     end 
 end

cb8ef21c8ee31a4d6f1a4e5c01e96d4decd9af2c
nkizito92 <nkizito57@gmail.com>
2020-06-03 09:39:28 -0500

    getting ready to deploy backend to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2020_04_20_161213) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "adminpack"
+  enable_extension "plpgsql"
+
   create_table "chats", force: :cascade do |t|
     t.string "message"
     t.string "guest_id"
